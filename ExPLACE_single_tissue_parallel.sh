bb56497076981972337f3dfdc7c35251ead44c24
jordanhughey <jmh791@psu.edu>
2021-04-14 16:37:38 -0400

    Version 1.0

---
+++ ExPLACE_single_tissue_parallel.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#Jordan Hughey
+#Liu Group
+
+tiss=lcl
+tissue=lymphoblastoid
+
+for i in `seq 21 22`
+do
+	chrom=${i}
+	echo "running chrom${chrom}"
+	prefix=${tiss}_ExPLACE_nested_cv
+	out_dir=`pwd`
+	cis_window=1e6
+	alpha=0.5
+	n_folds=10
+	n_train_test_folds=5
+	
+
+	expression_RDS=./input/test_exp_matrix_comb.RDS
+	geno_file=./input/test_genotype_matrix_chr${chrom}.txt
+	gene_annot_RDS=./input/gencode.v26lift37.annotation.gene.only.parsed.RDS
+	snp_annot_RDS=./input/test_snp_annot.chr${chrom}.RDS
+	covariates_file=NA
+	gene_snps_RDS=./input/snp-gene_pairs/GM12878_newABC_sig_snplist_1mb.RDS	        
+
+	qsub -d ${out_dir} -N ${tiss}_ExP_chr${chrom} -v chrom="${chrom}",prefix="${prefix}",out_dir="${out_dir}",cis_window="${cis_window}",alpha="${alpha}",n_folds="${n_folds}",n_train_test_folds="${n_train_test_folds}",expression_RDS="${expression_RDS}",geno_file="${geno_file}",gene_annot_RDS="${gene_annot_RDS}",snp_annot_RDS="${snp_annot_RDS}",covariates_file="${covariates_file}",gene_snps_RDS="${gene_snps_RDS}" ./scripts/ExPLACE_single_tissue.pbs 
+
+done
+
---
+++ ExPLACE_single_tissue_run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#Jordan Hughey
+#Liu Group
+
+tiss=lcl_test
+tissue=lymphoblastoid
+
+chrom=22
+echo "running chrom${chrom}"
+prefix=${tiss}_ExPLACE_nested_cv
+out_dir=`pwd`
+cis_window=1e6
+alpha=0.5
+n_folds=10
+n_train_test_folds=5
+	
+
+expression_RDS=./input/test_exp_matrix_chr22.RDS
+geno_file=./input/test_genotype_matrix_chr22.txt
+gene_annot_RDS=./input/gencode.v26lift37.annotation.gene.only.parsed.RDS
+snp_annot_RDS=./input/test_snp_annot.chr22.RDS
+covariates_file=NA
+gene_snps_RDS=./input/snp-gene_pairs/GM12878_newABC_sig_snplist_1mb.RDS
+
+Rscript ./scripts/ExPLACE_single_tissue.R ${chrom} ${prefix} ${out_dir} ${cis_window} ${alpha} ${n_folds} ${n_train_test_folds} \
+        ${expression_RDS} ${geno_file} ${gene_annot_RDS} ${snp_annot_RDS} ${covariates_file} ${gene_snps_RDS}
+
+
+
+

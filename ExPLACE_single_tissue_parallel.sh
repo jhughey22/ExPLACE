210b9fe0e91d35ea7b43cd35963ca72cce6a343c
jordanhughey <jmh791@psu.edu>
2021-04-15 13:33:46 -0400

    update README and inputs

--- ExPLACE_single_tissue_parallel.sh
+++ ExPLACE_single_tissue_parallel.sh
@@ -17,12 +17,12 @@ do
 	n_train_test_folds=5
 	
 
-	expression_RDS=./input/test_exp_matrix_comb.RDS
-	geno_file=./input/test_genotype_matrix_chr${chrom}.txt
-	gene_annot_RDS=./input/gencode.v26lift37.annotation.gene.only.parsed.RDS
-	snp_annot_RDS=./input/test_snp_annot.chr${chrom}.RDS
+	expression_RDS=./example_input/test_exp_matrix_comb.RDS
+	geno_file=./example_input/test_genotype_matrix_chr${chrom}.txt
+	gene_annot_RDS=./example_input/gencode.v26lift37.annotation.gene.only.parsed.RDS
+	snp_annot_RDS=./example_input/test_snp_annot.chr${chrom}.RDS
 	covariates_file=NA
-	gene_snps_RDS=./input/snp-gene_pairs/GM12878_newABC_sig_snplist_1mb.RDS	        
+	gene_snps_RDS=./example_input/snp-gene_pairs/GM12878_newABC_sig_snplist_1mb.RDS	        
 
 	qsub -d ${out_dir} -N ${tiss}_ExP_chr${chrom} -v chrom="${chrom}",prefix="${prefix}",out_dir="${out_dir}",cis_window="${cis_window}",alpha="${alpha}",n_folds="${n_folds}",n_train_test_folds="${n_train_test_folds}",expression_RDS="${expression_RDS}",geno_file="${geno_file}",gene_annot_RDS="${gene_annot_RDS}",snp_annot_RDS="${snp_annot_RDS}",covariates_file="${covariates_file}",gene_snps_RDS="${gene_snps_RDS}" ./scripts/ExPLACE_single_tissue.pbs 
 

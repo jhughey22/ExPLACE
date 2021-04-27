f68fa91407a1c86716eb39ac78301335516aa1c8
jordanhughey <jmh791@psu.edu>
2021-04-27 11:33:33 -0400

    comments and documentation on scripts

--- ExPLACE_single_tissue_run.sh
+++ ExPLACE_single_tissue_run.sh
@@ -2,26 +2,33 @@
 #Jordan Hughey
 #Liu Group
 
-tiss=lcl_test
+#This script is the wrapper to run ExPLACE model training in a single chromosome
+#All the options are hard coded below and can be changed for data of interest
+
+#Tissue the gene expression model training is performed
+tiss=lcl_test   #abbreviation for longer tissue name
 tissue=lymphoblastoid
 
 chrom=22
 echo "running chrom${chrom}"
 prefix=${tiss}_ExPLACE_nested_cv
-out_dir=`pwd`
-cis_window=1e6
-alpha=0.5
-n_folds=10
-n_train_test_folds=5
+out_dir=`pwd`  #outputs stored here
+cis_window=1e6	#Window for picking snps in proximity of gene
+alpha=0.5	#Elastic net mixing parameter
+n_folds=10	#Inner loop cross validation
+n_train_test_folds=5	#Outer loop folds
 	
 
 expression_RDS=./example_input/test_exp_matrix_chr22.RDS
 geno_file=./example_input/test_genotype_matrix_chr22.txt
 gene_annot_RDS=./example_input/gencode.v26lift37.annotation.gene.only.parsed.RDS
 snp_annot_RDS=./example_input/test_snp_annot.chr22.RDS
+#Covariates file; if there isn't one keep as NA
 covariates_file=NA
+#gene-snps pairs file; precomputed for 14 tissues in repo
 gene_snps_RDS=./example_input/snp-gene_pairs/GM12878_newABC_sig_snplist_1mb.RDS
 
+#Call to the Rscript which calls the ExPLACE function
 Rscript ./scripts/ExPLACE_single_tissue.R ${chrom} ${prefix} ${out_dir} ${cis_window} ${alpha} ${n_folds} ${n_train_test_folds} \
         ${expression_RDS} ${geno_file} ${gene_annot_RDS} ${snp_annot_RDS} ${covariates_file} ${gene_snps_RDS}
 

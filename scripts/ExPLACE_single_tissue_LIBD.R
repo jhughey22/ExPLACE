669f4c0d35e1971594b31eaa72144850cc347d21
jordanhughey <jmh791@psu.edu>
2021-06-28 13:51:34 -0400

    adjustment check

--- scripts/ExPLACE_single_tissue_LIBD.R
+++ scripts/ExPLACE_single_tissue_LIBD.R
@@ -6,7 +6,8 @@
 
 #Takes args from wrapper scripts in repo
 argv <- commandArgs(trailingOnly = TRUE)
-source('./scripts/gtex_v7_nested_cv_elnet_weighted_hybrid_LIBD.R')
+#source('./scripts/gtex_v7_nested_cv_elnet_weighted_hybrid_LIBD.R')
+source('./scripts/gtex_v7_nested_cv_elnet_weighted_hybrid_LIBD_v4.R')
 
 chrom <- argv[1]
 prefix <- argv[2]
